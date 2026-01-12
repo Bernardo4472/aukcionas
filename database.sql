befee38abd8955efe76d614bd6a5ed57844530a2
Rokas Kaziulis <bernardo4472@gmail.com>
2026-01-12 13:37:07 +0200

    Update auction dates in database.sql to 2026 for active listings

--- database.sql
+++ database.sql
@@ -187,13 +187,13 @@ INSERT INTO vartotojai (vardas, el_pastas, slaptazodis, role, balansas) VALUES
 
 -- Demonstraciniai aukcionai
 INSERT INTO aukcionai (pavadinimas, aprasymas, pradine_kaina, dabartine_kaina, bid_step, pradzios_laikas, pabaigos_laikas, pasleptas, vartotojo_id, busena) VALUES
-('iPhone 15 Pro Max', 'Naujas, naudotas tik 2 mėnesius. Su dėklu ir apsaugine plėvele.', 800.00, 850.00, 10.00, '2025-11-20 10:00:00', '2025-12-05 18:00:00', FALSE, 3, 'aktyvus'),
-('Samsung Galaxy S24', 'Originalus, su visais priedais. Puiki būklė.', 600.00, 650.00, 15.00, '2025-11-22 12:00:00', '2025-12-10 20:00:00', FALSE, 5, 'aktyvus'),
-('MacBook Pro 2024', '16 coliųM3 Max, 32GB RAM, 1TB SSD. Idealus programuotojams.', 2000.00, 2100.00, 50.00, '2025-11-25 09:00:00', '2025-12-15 21:00:00', FALSE, 6, 'aktyvus'),
-('Sony PlayStation 5', 'Nenaudotas, dar su garantija. 2 pulteliai.', 400.00, 450.00, 10.00, '2025-11-18 14:00:00', '2025-12-01 17:00:00', FALSE, 3, 'aktyvus'),
-('Vintage laikrodis Rolex', 'Kolekcinė prekė. Autentiškas, su sertifikatu.', 5000.00, 5000.00, 100.00, '2025-11-28 08:00:00', '2025-12-20 23:59:59', FALSE, 5, 'aktyvus'),
-('Dell XPS 13', 'Nešiojamas kompiuteris, i7 procesorius, 16GB RAM.', 900.00, 900.00, 20.00, '2025-11-27 10:00:00', '2025-12-08 19:00:00', FALSE, 6, 'aktyvus'),
-('Slaptas aukcionas', 'Šis aukcionas matomas tik savininkui ir administratoriui.', 100.00, 100.00, 5.00, '2025-11-26 15:00:00', '2025-12-12 16:00:00', TRUE, 3, 'aktyvus');
+('iPhone 15 Pro Max', 'Naujas, naudotas tik 2 mėnesius. Su dėklu ir apsaugine plėvele.', 800.00, 850.00, 10.00, '2026-01-10 10:00:00', '2026-02-05 18:00:00', FALSE, 3, 'aktyvus'),
+('Samsung Galaxy S24', 'Originalus, su visais priedais. Puiki būklė.', 600.00, 650.00, 15.00, '2026-01-11 12:00:00', '2026-02-10 20:00:00', FALSE, 5, 'aktyvus'),
+('MacBook Pro 2024', '16 colių M3 Max, 32GB RAM, 1TB SSD. Idealus programuotojams.', 2000.00, 2100.00, 50.00, '2026-01-08 09:00:00', '2026-02-15 21:00:00', FALSE, 6, 'aktyvus'),
+('Sony PlayStation 5', 'Nenaudotas, dar su garantija. 2 pulteliai.', 400.00, 450.00, 10.00, '2026-01-05 14:00:00', '2026-02-01 17:00:00', FALSE, 3, 'aktyvus'),
+('Vintage laikrodis Rolex', 'Kolekcinė prekė. Autentiškas, su sertifikatu.', 5000.00, 5000.00, 100.00, '2026-01-12 08:00:00', '2026-02-20 23:59:59', FALSE, 5, 'aktyvus'),
+('Dell XPS 13', 'Nešiojamas kompiuteris, i7 procesorius, 16GB RAM.', 900.00, 900.00, 20.00, '2026-01-09 10:00:00', '2026-02-08 19:00:00', FALSE, 6, 'aktyvus'),
+('Slaptas aukcionas', 'Šis aukcionas matomas tik savininkui ir administratoriui.', 100.00, 100.00, 5.00, '2026-01-07 15:00:00', '2026-02-12 16:00:00', TRUE, 3, 'aktyvus');
 
 -- Demonstraciniai statymai
 INSERT INTO statymai (aukciono_id, vartotojo_id, suma, data_laikas) VALUES
